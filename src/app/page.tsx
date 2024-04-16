7a3ae6b19dbbb1ec09888ffd41a5f67bcbd48622
Eduardo Aguilar <Agu1laredu@hotmail.com>
2024-04-16 14:49:36 -0300

    dashboarddd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,19 +47,19 @@ export default async function Home() {
           </div>
           <nav className="flex-1 overflow-y-auto">
             <ul className="p-2 space-y-2">
-              <li className="font-bold flex items-center pl-4 py-2 text-base hover:bg-blue-700">
+              <li className="font-bold flex items-center pl-4 py-2 text-base hover:bg-blue-700 cursor-pointer">
                 <AiTwotoneHome className="w-5 h-5 mr-3" />
                 <span>INICIO</span>
               </li>
-              <li className="font-bold flex items-center pl-4 py-2 text-base hover:bg-blue-700">
+              <li className="font-bold flex items-center pl-4 py-2 text-base hover:bg-blue-700 cursor-pointer">
                 <FaMoneyCheckDollar className="w-5 h-5 mr-3" />
                 <span>FACTURACIÓN</span>
               </li>
-              <li className="font-bold flex items-center pl-4 py-2 text-base hover:bg-blue-700">
+              <li className="font-bold flex items-center pl-4 py-2 text-base hover:bg-blue-700 cursor-pointer">
                 <MdInventory className="w-5 h-5 mr-3" />
                 <span>INVENTARIO</span>
               </li>
-              <li className="font-bold flex items-center pl-4 py-2 text-base hover:bg-blue-700">
+              <li className="font-bold flex items-center pl-4 py-2 text-base hover:bg-blue-700 cursor-pointer">
                 <TbReportSearch className="w-5 h-5 mr-3" />
                 <span>REPORTES</span>
               </li>
